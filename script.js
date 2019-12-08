f23b158a239fd189de1bb0bec398ba1e2e440b2a
ryan woods <federer5050@gmail.com>
2019-12-08 15:16:57 +0000

    utility functions

--- script.js
+++ script.js
@@ -45,3 +45,63 @@ form.addEventListener('submit', function(event) {
     }, 1000);
   }
 });
+
+// Validators
+function validateFirstName() {
+  // check if is empty
+  if (checkIfEmpty(firstName)) return;
+  // is if it has only letters
+  if (!checkIfOnlyLetters(firstName)) return;
+  return true;
+}
+function validateLastName() {
+  // check if is empty
+  if (checkIfEmpty(lastName)) return;
+  // is if it has only letters
+  if (!checkIfOnlyLetters(lastName)) return;
+  return true;
+}
+function validatePassword() {
+  // Empty check
+  if (checkIfEmpty(password)) return;
+  // Must of in certain length
+  if (!meetLength(password, 4, 100)) return;
+  // check password against our character set
+  // 1- a
+  // 2- a 1
+  // 3- A a 1
+  // 4- A a 1 @
+  //   if (!containsCharacters(password, 4)) return;
+  return true;
+}
+function validateConfirmPassword() {
+  if (password.className !== 'valid') {
+    setInvalid(confirmPassword, 'Password must be valid');
+    return;
+  }
+  // If they match
+  if (password.value !== confirmPassword.value) {
+    setInvalid(confirmPassword, 'Passwords must match');
+    return;
+  } else {
+    setValid(confirmPassword);
+  }
+  return true;
+}
+function validateEmail() {
+  if (checkIfEmpty(email)) return;
+  if (!containsCharacters(email, 5)) return;
+  return true;
+}
+// Utility functions
+function checkIfEmpty(field) {
+  if (isEmpty(field.value.trim())) {
+    // set field invalid
+    setInvalid(field, `${field.name} must not be empty`);
+    return true;
+  } else {
+    // set field valid
+    setValid(field);
+    return false;
+  }
+}

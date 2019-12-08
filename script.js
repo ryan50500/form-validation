d528ebeb6978b437d647f6b2fcfee5e03575b930
ryan woods <federer5050@gmail.com>
2019-12-08 13:50:30 +0000

    validate form

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ const form = document.getElementById('myForm');
 // Validation colors
 const green = '#4CAF50';
 const red = '#F44336';
+
+// Handle form
+form.addEventListener('submit', function(event) {
+  // Prevent default behaviour
+  event.preventDefault();
+  if (
+    validateFirstName() &&
+    validateLastName() &&
+    validatePassword() &&
+    validateConfirmPassword() &&
+    validateEmail()
+  ) {
+    const name = firstName.value;
+    const container = document.querySelector('div.container');
+    const loader = document.createElement('div');
+    loader.className = 'progress';
+    const loadingBar = document.createElement('div');
+    loadingBar.className = 'indeterminate';
+    loader.appendChild(loadingBar);
+    container.appendChild(loader);
+    setTimeout(function() {
+      const loaderDiv = document.querySelector('div.progress');
+      const panel = document.createElement('div');
+      panel.className = 'card-panel green';
+      const text = document.createElement('span');
+      text.className = 'white-text';
+      text.appendChild(
+        document.createTextNode(
+          `Sign up successful, welcom to SocialApe ${name}`
+        )
+      );
+      panel.appendChild(text);
+      container.replaceChild(panel, loaderDiv);
+    }, 1000);
+  }
+});

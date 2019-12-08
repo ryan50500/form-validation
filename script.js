e2ab31f153ddbd7975b40764f3962319c245097f
ryan woods <federer5050@gmail.com>
2019-12-08 13:25:05 +0000

    declaring variables/ input fields

---
+++ script.js
@@ -0,0 +1,11 @@
+// Input fields
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const email = document.getElementById('firstName');
+// Form
+const form = document.getElementById('myForm');
+// Validation colors
+const green = '#4CAF50';
+const red = '#F44336';

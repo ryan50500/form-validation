3fef21c164270f98fa156421c1e958b8e6bd2c0d
ryan woods <federer5050@gmail.com>
2020-09-08 12:32:48 +0100

    console log successful

--- script.js
+++ script.js
@@ -20,27 +20,9 @@ form.addEventListener('submit', function(event) {
     validatePassword() &&
     validateConfirmPassword()
   ) {
-    const name = firstName.value;
-    const container = document.querySelector('div.container');
-    const loader = document.createElement('div');
-    loader.className = 'progress';
-    const loadingBar = document.createElement('div');
-    loadingBar.className = 'indeterminate';
-    loader.appendChild(loadingBar);
-    container.appendChild(loader);
+
     setTimeout(function() {
-      const loaderDiv = document.querySelector('div.progress');
-      const panel = document.createElement('div');
-      panel.className = 'card-panel green';
-      const text = document.createElement('span');
-      text.className = 'white-text';
-      text.appendChild(
-        document.createTextNode(
-          `Sign up successful, welcom to SocialApe ${name}`
-        )
-      );
-      panel.appendChild(text);
-      container.replaceChild(panel, loaderDiv);
+      console.log ('validation successful');
     }, 1000);
   }
 });
